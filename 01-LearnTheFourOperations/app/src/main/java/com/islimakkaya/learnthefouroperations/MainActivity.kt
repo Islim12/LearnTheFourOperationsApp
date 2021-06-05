8c6f529e3e39bfa1ad76215d9ebe0483a0c5165d
Islim12 <44882584+Islim12@users.noreply.github.com>
2021-06-06 01:38:05 +0300

    Update MainActivity.kt

--- 01-LearnTheFourOperations/app/src/main/java/com/islimakkaya/learnthefouroperations/MainActivity.kt
+++ 01-LearnTheFourOperations/app/src/main/java/com/islimakkaya/learnthefouroperations/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        //val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         //NavigationUI.setupWithNavController(bottomNav,navHostFragment.navController)
     }
 }
